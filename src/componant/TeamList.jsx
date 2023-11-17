6f274b131a4b9f5d96c372acdf69b4c9d0058279
Lucile Bouchire <l.bouchire@gmail.com>
2023-11-17 16:56:45 +0100

    adding images

--- src/componant/TeamList.jsx
+++ src/componant/TeamList.jsx
@@ -1,34 +1,45 @@
 import { Link } from "react-router-dom";
 import Header from "./Header";
 
+import "./TeamList.scss";
+
 function TeamList() {
   const trainers = [
     {
       id: 1,
       name: "Sasha du Bourg Palette",
+      img: "https://fr.web.img3.acsta.net/r_654_368/newsv7/20/03/19/17/33/4454395.jpg",
       bio: "Je veux tous les attraper",
     },
     {
       id: 2,
       name: "Dwayne Johnson",
+      img: "https://www.numerama.com/wp-content/uploads/2023/05/regis-pokemon.jpg",
       bio: "Je veux tous les soulever",
     },
-    { id: 3, name: "Affida Turner", bio: "Je veux décéder" },
+    {
+      id: 3,
+      name: "Affida Turner",
+      img: "https://assets.pokemon.com/assets/cms2/img/watch-pokemon-tv/seasons/season06/season06_ep47_ss02.jpg",
+      bio: "Je veux décéder",
+    },
   ];
 
   return (
     <>
-      <h2>Les entrainteurs</h2>
-
-      {trainers.map((trainer) => {
-        return (
-          <Link to={`/trainers/${trainer.id}/details`}>
-            <article>
-              <h2>{trainer.name}</h2>
-            </article>
-          </Link>
-        );
-      })}
+      <h2>Les entrainteurs : </h2>
+      <div className="trainer-list">
+        {trainers.map((trainer) => {
+          return (
+            <Link to={`/trainers/${trainer.id}/details`}>
+              <article className="trainer-article">
+                <h2>{trainer.name}</h2>
+                <img src={trainer.img} alt={trainer.name}></img>
+              </article>
+            </Link>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/page/TrainerDetailPage.jsx
+++ src/page/TrainerDetailPage.jsx
@@ -6,14 +6,21 @@ function TrainerDetailPage() {
     {
       id: 1,
       name: "Sasha du Bourg Palette",
+      img: "https://fr.web.img3.acsta.net/r_654_368/newsv7/20/03/19/17/33/4454395.jpg",
       bio: "Je veux tous les attraper",
     },
     {
       id: 2,
       name: "Dwayne Johnson",
+      img: "https://www.numerama.com/wp-content/uploads/2023/05/regis-pokemon.jpg",
       bio: "Je veux tous les soulever",
     },
-    { id: 3, name: "Affida Turner", bio: "Je veux décéder" },
+    {
+      id: 3,
+      name: "Affida Turner",
+      img: "https://assets.pokemon.com/assets/cms2/img/watch-pokemon-tv/seasons/season06/season06_ep47_ss02.jpg",
+      bio: "Je veux décéder",
+    },
   ];
 
   const { trainerId } = useParams();
@@ -30,6 +37,7 @@ function TrainerDetailPage() {
       <h2>Voici le détail sur l'entraineur sélectionné :</h2>
       <main>
         <h3>{trainerToDisplay.name}</h3>
+        <img src={trainerToDisplay.img} alt={trainerToDisplay.name}></img>
         <p>Sa bio : "{trainerToDisplay.bio}"</p>
       </main>
     </>
